09a54dcb9a85413543308f785789288bc4b8c795
coungdev <coungdev@gmail.com>
2023-11-08 00:16:25 +0700

    implement the Valuer/Scanner interface for Image

--- internal/common/image.go
+++ internal/common/image.go
@@ -1,5 +1,12 @@
 package common
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
 type Image struct {
 	// Id     int    `json:"id" gorm:"column:id;"`
 	Url       string `json:"url" gorm:"column:url;"`
@@ -9,6 +16,28 @@ type Image struct {
 	CloudName string `json:"cloud_name" gorm:"column:cloud_name"`
 }
 
-// func (Image) TableName() string {
-// 	return "images"
-// }
+func (Image) TableName() string {
+	return "images"
+}
+
+func (j *Image) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+
+	var img Image
+	if err := json.Unmarshal(bytes, &img); err != nil {
+		return err
+	}
+
+	*j = img
+	return nil
+}
+
+func (j *Image) Value() (driver.Value, error) {
+	if j == nil {
+		return nil, nil
+	}
+	return json.Marshal(j)
+}

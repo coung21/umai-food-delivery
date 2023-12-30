9a3f2d47a3407168d130c03522366e31e7d44c94
coungdev <coungdev@gmail.com>
2023-12-30 12:22:34 +0700

    Add Location field to Restaurant and RestaurantUpdate models

--- internal/auth-service/model/restaurant.go
+++ internal/auth-service/model/restaurant.go
@@ -13,6 +13,7 @@ type Restaurant struct {
 	Cover          *common.Images `json:"cover" gorm:"column:cover"`
 	OpenHour       *time.Time     `json:"open_hour" gorm:"column:open_hour"`
 	CloseHour      *time.Time     `json:"close_hour" gorm:"column:close_hour"`
+	Location       Location       `json:"location" gorm:"column:location"`
 	Rating         float32        `json:"rating" gorm:"column:rating"`
 }
 
@@ -26,4 +27,11 @@ type RestaurantUpdate struct {
 	Cover          *common.Images `json:"cover" gorm:"column:cover"`
 	OpenHour       *time.Time     `json:"open_hour" gorm:"column:open_hour"`
 	CloseHour      *time.Time     `json:"close_hour" gorm:"column:close_hour"`
+	Location       *Location      `json:"location" gorm:"column:location"`
+}
+
+type Location struct {
+	Address string  `json:"address" gorm:"column:address"`
+	Lat     float64 `json:"lat" gorm:"column:lat"`
+	Lng     float64 `json:"lng" gorm:"column:lng"`
 }

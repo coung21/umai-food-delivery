141f0d7ff53751322fdd5596f1b2d136cb8e8d50
coungdev <coungdev@gmail.com>
2024-01-03 15:44:58 +0700

    Refactor Shipper model and add WorkZone struct
    This commit refactors the Shipper model in the auth-service package. It adds a new struct called WorkZone to represent the work zone of a shipper. The Shipper model now includes the WorkZone struct as a field. Additionally, the commit includes the implementation of the Scan and Value methods for the WorkZone struct to handle JSON marshaling and unmarshaling. This refactoring improves the organization and readability of the code.

--- internal/auth-service/model/shipper.go
+++ internal/auth-service/model/shipper.go
@@ -1,20 +1,23 @@
 package model
 
-import "time"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+)
 
 type Shipper struct {
-	ID           int    `json:"id" gorm:"column:id;primary_key;auto_increment"`
-	UserID       int    `json:"user_id" gorm:"column:user_id;not null;index"`
-	VehicleName  string `json:"vehicle_name" gorm:"column:vehicle_name;not null"`
-	VehicleType  string `json:"vehicle_type" gorm:"column:vehicle_type;not null"`
-	LicensePlate string `json:"license_plate" gorm:"column:license_plate;not null"`
-	WorkZone     struct {
-		Province string `json:"province" gorm:"column:province;not null"`
-		District string `json:"district" gorm:"column:district;not null"`
-	} `json:"work_zone" gorm:"column:work_zone;not null;type:json"`
-	WorkStatus string    `json:"work_status" gorm:"column:work_status;not null"`
-	UpdateAt   time.Time `json:"update_at" gorm:"column:update_at;not null"`
-	CreateAt   time.Time `json:"create_at" gorm:"column:create_at;not null"`
+	ID           int       `json:"id" gorm:"column:id;primary_key;auto_increment"`
+	UserID       int       `json:"user_id" gorm:"column:user_id;not null;index"`
+	VehicleName  string    `json:"vehicle_name" gorm:"column:vehicle_name;not null"`
+	VehicleType  string    `json:"vehicle_type" gorm:"column:vehicle_type;not null"`
+	LicensePlate string    `json:"license_plate" gorm:"column:license_plate;not null"`
+	WorkStatus   string    `json:"work_status" gorm:"column:work_status;not null"`
+	WorkZone     WorkZone  `json:"work_zone" gorm:"column:work_zone;not null;type:json"`
+	UpdateAt     time.Time `json:"update_at" gorm:"column:update_at;not null"`
+	CreateAt     time.Time `json:"create_at" gorm:"column:create_at;not null"`
 }
 
 func (Shipper) TableName() string {
@@ -22,14 +25,16 @@ func (Shipper) TableName() string {
 }
 
 type ShipperUpdate struct {
-	VehicleName  *string `json:"vehicle_name" gorm:"column:vehicle_name;not null"`
-	VehicleType  *string `json:"vehicle_type" gorm:"column:vehicle_type;not null"`
-	LicensePlate *string `json:"license_plate" gorm:"column:license_plate;not null"`
-	WorkZone     struct {
-		Province *string `json:"province" gorm:"column:province;not null"`
-		District *string `json:"district" gorm:"column:district;not null"`
-	} `json:"work_zone" gorm:"column:work_zone;not null;type:json"`
-	WorkStatus *string `json:"work_status" gorm:"column:work_status;not null"`
+	VehicleName  *string   `json:"vehicle_name" gorm:"column:vehicle_name;not null"`
+	VehicleType  *string   `json:"vehicle_type" gorm:"column:vehicle_type;not null"`
+	LicensePlate *string   `json:"license_plate" gorm:"column:license_plate;not null"`
+	WorkStatus   *string   `json:"work_status" gorm:"column:work_status;not null"`
+	WorkZone     *WorkZone `json:"work_zone" gorm:"column:work_zone;not null;type:json"`
+}
+
+type WorkZone struct {
+	Province string `json:"province" gorm:"column:province;not null"`
+	District string `json:"district" gorm:"column:district;not null"`
 }
 
 func (s *Shipper) UpdateStatus(status string) {
@@ -62,6 +67,23 @@ func (s *Shipper) IsBicycle() bool {
 	return s.VehicleType == ShipperVehicleTypeBicycle
 }
 
-func (s *Shipper) IsInWorkZone(province, district string) bool {
-	return s.WorkZone.Province == province && s.WorkZone.District == district
+func (s *WorkZone) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+
+	var workZone WorkZone
+	if err := json.Unmarshal(bytes, &workZone); err != nil {
+		return err
+	}
+	*s = workZone
+	return nil
+}
+
+func (s *WorkZone) Value() (driver.Value, error) {
+	if s == nil {
+		return nil, nil
+	}
+	return json.Marshal(s)
 }
